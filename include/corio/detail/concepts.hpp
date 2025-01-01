c62f89653893d4e683082a8b6c018d60f0fca6dd
wokron <stringcatwok@gmail.com>
2025-01-01 22:55:49 +0800

    Add awaitable concept

---
+++ include/corio/detail/concepts.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <coroutine>
+#include <unistd.h>
+
+namespace corio::detail {
+
+template <typename Awaiter, typename Promise = void>
+concept awaiter =
+    requires(Awaiter awaiter, std::coroutine_handle<Promise> handle) {
+        { awaiter.await_ready() };
+        { awaiter.await_suspend(handle) };
+        { awaiter.await_resume() };
+    };
+
+template <typename Awaitable, typename Promise = void>
+concept awaitable =
+    awaiter<Awaitable, Promise> || requires(Awaitable awaiter) {
+                                     { awaiter.operator co_await() };
+                                 };
+
+} // namespace corio::detail
