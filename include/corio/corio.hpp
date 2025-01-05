2de5b357b30ed8e29a275ae065a860f664e7377c
wokron <stringcatwok@gmail.com>
2025-01-05 21:51:27 +0800

    Add corio.hpp header to include all

---
+++ include/corio/corio.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "corio/any_awaitable.hpp"
+#include "corio/exceptions.hpp"
+#include "corio/gather.hpp"
+#include "corio/generator.hpp"
+#include "corio/lazy.hpp"
+#include "corio/result.hpp"
+#include "corio/runner.hpp"
+#include "corio/select.hpp"
+#include "corio/task.hpp"
+#include "corio/this_coro.hpp"
