d17b10b654f77129c0b98893abb5d062de3b46d0
wokron <stringcatwok@gmail.com>
2025-01-16 10:56:55 +0800

    Update spawn test

--- bench/spawn.cpp
+++ bench/spawn.cpp
@@ -5,21 +5,14 @@
 
 constexpr std::size_t n = 1'000'000;
 
-corio::Lazy<void> corio_task() {
-    static int count = 0;
-    count++;
-    co_return;
-}
+corio::Lazy<void> corio_task() { co_return; }
 
 corio::Lazy<void> corio_test() {
     auto ex = co_await corio::this_coro::executor;
 
-    std::vector<corio::Task<void>> tasks;
     for (std::size_t i = 0; i < n; i++) {
-        tasks.push_back(corio::spawn(ex, corio_task()));
+        corio::spawn_background(ex, corio_task());
     }
-
-    co_await corio::gather(std::move(tasks));
 }
 
 void launch_corio_test() {
@@ -28,25 +21,15 @@ void launch_corio_test() {
     ctx.run();
 }
 
-asio::awaitable<void> asio_task() {
-    static int count = 0;
-    count++;
-    co_return;
-}
+asio::awaitable<void> asio_task() { co_return; }
 
 asio::awaitable<void> asio_test() {
     auto ex = co_await asio::this_coro::executor;
 
     using Task = decltype(asio::co_spawn(ex, asio_task(), asio::deferred));
-    std::vector<Task> tasks;
     for (std::size_t i = 0; i < n; i++) {
-        tasks.push_back(asio::co_spawn(ex, asio_task(), asio::deferred));
+        asio::co_spawn(ex, asio_task(), asio::detached);
     }
-
-    auto group = asio::experimental::make_parallel_group(std::move(tasks));
-
-    co_await group.async_wait(asio::experimental::wait_for_all(),
-                              asio::use_awaitable);
 }
 
 void launch_asio_test() {
@@ -57,13 +40,13 @@ void launch_asio_test() {
 
 int main() {
     for (std::size_t i = 0; i < 6; i++) {
-        auto dur = marker::measured(launch_asio_test)();
-        std::cerr << "asio: " << dur << std::endl;
+        auto dur = marker::measured(launch_corio_test)();
+        std::cerr << "corio: " << dur << std::endl;
     }
 
     for (std::size_t i = 0; i < 6; i++) {
-        auto dur = marker::measured(launch_corio_test)();
-        std::cerr << "corio: " << dur << std::endl;
+        auto dur = marker::measured(launch_asio_test)();
+        std::cerr << "asio: " << dur << std::endl;
     }
 
     return 0;
